f6e8289e2c724bd6a1e77b8f8b1088be779610f5
Morim Adrien <adrien.986@gmail.com>
2019-05-16 17:35:39 +0200

    Modif pour ajouter les champs via les fixtures

--- src/Controller/WorkController.php
+++ src/Controller/WorkController.php
@@ -4,23 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Work;
+use App\Repository\WorkRepository;
 
 class WorkController extends AbstractController
 {
     /**
      * @Route("/work/list", name="work_list")
      */
-    public function listWork()
+    public function listWork(WorkRepository $repo)
     {
+        $works = $repo->findAll();
+
         return $this->render('work/list.html.twig', [
             'controller_name' => 'WorkController',
+            'works' => $works
         ]);
     }
     /**
-     * @Route("/work/12", name="work_show")
+     * @Route("/work/{id}", name="work_show")
      */
-    public function showWork()
+    public function showWork(Work $work)
     {
-        return $this->render('work/show.html.twig');
+        return $this->render('work/show.html.twig', [
+            'work' => $work
+        ]);
     }
 }
