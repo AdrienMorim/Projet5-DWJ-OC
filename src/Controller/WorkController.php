ba3781b3e1f69f74e31ec8f98bc4fe444fbc4aad
Morim Adrien <adrien.986@gmail.com>
2019-05-17 12:43:04 +0200

    Ajout de la fonction formWork

--- src/Controller/WorkController.php
+++ src/Controller/WorkController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
 use App\Entity\Work;
 use App\Repository\WorkRepository;
+use App\Form\WorkType;
 
 class WorkController extends AbstractController
 {
@@ -21,6 +25,38 @@ class WorkController extends AbstractController
             'works' => $works
         ]);
     }
+
+    /**
+     * @Route("/work/new", name="work_create")
+     * @Route("/work/{id}/edit", name="work_edit")
+     */
+    public function formWork(Work $work = null, Request $request, ObjectManager $manager)
+    {
+        if(!$work) {
+            $work = new Work();
+        }
+
+        $form = $this->createForm(WorkType::class, $work);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$work->getId()){
+                $work->setCreatedAt(new \Datetime());
+            }
+
+            $manager->persist($work);
+            $manager->flush();
+
+            return $this->redirectToRoute('work_show', ['id' => $work->getId()]);
+        } 
+
+        return $this->render('work/create.html.twig' ,[
+            'formWork' => $form->createView(),
+            'editMode' => $work->getId() !== null
+        ]);
+    }
+
     /**
      * @Route("/work/{id}", name="work_show")
      */
