36c1ee9a0d6698098a720f07a8c606e6908f9030
Morim Adrien <adrien.986@gmail.com>
2019-05-20 12:13:34 +0200

    Optimisation avec Faker, ajout de catégorie

--- src/DataFixtures/WorkFixtures.php
+++ src/DataFixtures/WorkFixtures.php
@@ -5,23 +5,36 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Work;
+use App\Entity\Category;
 
 class WorkFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++){
-            $work = new Work();
-            $work->setTitle("Titre $i")
-                 ->setSubtitle("Sous-titre $i")
-                 ->setContent("Contenu de $i")
-                 ->setLink("Lien $i")
-                 ->setImage("http://via.placeholder.com/640x360")
-                 ->setImageAlt("Description de $i")
-                 ->setCreatedAt(new \DateTime());
-            $manager->persist($work);
-        }
+        $faker = \Faker\Factory::create('fr_FR');
 
+        // Créer entre 6 à 10 projets
+        for($i = 1; $i <= mt_rand(6, 10); $i++){
+            $work = new Work();
+            $work->setTitle($faker->word())
+                 ->setSubtitle($faker->sentence(3))
+                 ->setContent($faker->paragraph())
+                 ->setLink($faker->url())
+                 ->setImage($faker->imageUrl())
+                 ->setImageAlt($faker->sentence())
+                 ->setCreatedAt($faker->dateTimeBetween('- 6 month'));
+
+            $manager->persist($work);
+
+            // Créer entre 3 à 5 catégories
+            for($j = 1; $j <= mt_rand(3, 5); $j++){
+                $category = new Category();
+                $category->setTitle($faker->word())
+                        ->addWork($work);
+
+                $manager->persist($category);
+            }
+        }
         $manager->flush();
     }
 }
