e0974051274607f6ea05997841d81f4588baa45c
Morim Adrien <adrien.986@gmail.com>
2019-05-22 22:41:07 +0200

    Ajout des categories sous forme de checkbox dans le form

--- src/Form/WorkType.php
+++ src/Form/WorkType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Work;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class WorkType extends AbstractType
 {
@@ -18,13 +20,20 @@ class WorkType extends AbstractType
             ->add('link')
             ->add('image')
             ->add('imageAlt')
+            ->add('categories', EntityType::class, [ // On précise d'où vient les catégories
+                'class' => Category::class,
+                'choice_label' => 'title',
+                'expanded' => true,
+                'multiple' => true,
+                'by_reference' => false // A utilisé pour que addCategory() / removeCategory() soient appelés comme categories est un ArrayCollection()
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Work::class,
+            'data_class' => Work::class
         ]);
     }
 }
