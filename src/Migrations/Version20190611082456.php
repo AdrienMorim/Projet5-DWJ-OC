ce8011ccaaed90d3ee0a8e6484a056eb2c680af1
Morim Adrien <adrien.986@gmail.com>
2019-06-11 10:30:53 +0200

    MAJ de la db

---
+++ src/Migrations/Version20190611082456.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190611082456 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE work DROP image, DROP image_alt');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE work ADD image VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, ADD image_alt VARCHAR(60) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
