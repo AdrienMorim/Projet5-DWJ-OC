09222f417a87e0ad76b655150699f5d9b351e017
Morim Adrien <adrien.986@gmail.com>
2019-06-13 23:37:52 +0200

    Ajout de UniqueEntity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,13 +2,17 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @UniqueEntity(
+ *      fields = {"name"},
+ *      message = "Le nom '{{ value }}' que vous avez saisie existe déjà !")
  */
 class Category
 {
