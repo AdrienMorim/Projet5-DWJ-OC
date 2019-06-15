a5e3bcc1cb9c28b3c5fa58bd32615cc144ab04d7
Morim Adrien <adrien.986@gmail.com>
2019-06-15 13:53:30 +0200

    Ajout des repo nécessaire

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -6,23 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\WorkRepository;
+use App\Repository\CategoryRepository;
+use App\Entity\Category;
 
 class PortfolioController extends AbstractController
 {
     /**
      * @Route("/", name="portfolio")
      */
-    public function index(WorkRepository $repo)
+    public function index(WorkRepository $repoWork, CategoryRepository $repoCategory)
     {
-        $works = $repo->findAll();
+        $works = $repoWork->findAll();
+        $categories = $repoCategory->findAll();
+        $firstCategory = $repoCategory->find(1);
 
         return $this->render('portfolio/index.html.twig', [
             'controller_name' => 'PortfolioController',
-            'works' => $works
+            'works' => $works,
+            'categories' => $categories,
+            'firstCategory' => $firstCategory
         ]);
     }
     /**
-     * @Route("/dashboard", name="portfolio_dashboard")
+     * @Route("/admin/dashboard", name="portfolio_dashboard")
      */
     public function dashboard(WorkRepository $repo)
     {
