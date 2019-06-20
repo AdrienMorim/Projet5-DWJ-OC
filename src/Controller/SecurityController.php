45e4ef378965d5b754995b5236904cd89ae710f9
Morim Adrien <adrien.986@gmail.com>
2019-06-20 17:19:20 +0200

    Ajout de gestion user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\EditType;
+
 use App\Form\RegistrationType;
 
+use App\Form\ResetPasswordType;
 use Symfony\Component\HttpFoundation\Request;
-
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -21,12 +24,13 @@ class SecurityController extends AbstractController
     public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
-
+        
         $form = $this->createForm(RegistrationType::class, $user); // Champs du formulaire binder (relier) avec ceux de l'utilisateur
 
         $form->handleRequest($request); // Analyse la requête HTTP
 
         if($form->isSubmitted() && $form->isValid()) {
+
             $hash = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hash);
             $user->setRoles(['ROLE_USER']);
@@ -62,5 +66,52 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="security_logout")
      */
-    public function logout() {}
+    public function logout() {} 
+
+    /**
+     * @Route("/admin/settings", name="security_settings")
+     * @Route("/admin/user/{id}/edit", name="security_edit", requirements={"id"="\d+"})
+     *
+     * @param UserInterface $user
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return void
+     */
+    public function editUser(UserInterface $user, Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
+
+        $form = $this->createForm(EditType::class, $user);
+
+        $form->handleRequest($request); // Analyse la requête HTTP
+
+        if($form->isSubmitted() && $form->isValid()) {
+            
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('success', 'Les modifications ont bien été enregistré');
+
+            return $this->redirectToRoute('security_settings');
+        }
+
+        $formResetPassword = $this->createForm(ResetPasswordType::class, $user);
+
+        $formResetPassword->handleRequest($request);
+
+        if($formResetPassword->isSubmitted() && $formResetPassword->isValid()) {
+            
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('success', 'Le mot de passe a bien été modifié');
+
+            return $this->redirectToRoute('security_settings');
+        }
+
+        return $this->render('security/settings.html.twig', [
+            'formEdit' => $form->createView(),
+            'formResetPassword' => $formResetPassword->createView()
+        ]);
+    }
 }
