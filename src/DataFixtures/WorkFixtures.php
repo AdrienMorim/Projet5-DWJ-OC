a8ef1904c33e640754f81e4d0017d1d356324c22
Morim Adrien <adrien.986@gmail.com>
2019-06-25 14:06:29 +0200

    Correctif Fixtures

--- src/DataFixtures/WorkFixtures.php
+++ src/DataFixtures/WorkFixtures.php
@@ -8,6 +8,7 @@ use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class WorkFixtures extends Fixture
 {
@@ -18,6 +19,8 @@ class WorkFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        
+        /*
         $faker = \Faker\Factory::create('fr_FR');
 
         // Créer entre 1 à 5 catégories
@@ -40,7 +43,171 @@ class WorkFixtures extends Fixture
 
                 $manager->persist($work);
             }
-        }
+        }*/
+
+        // On crée les projets avec les catégories attribués
+
+        // 1st
+        $work = new Work();
+
+        $work->setTitle("Webagency")
+                ->setSubtitle("site de l'agence web")
+                ->setContent("Développeur / Intégrateur HTML5/CSS3")
+                ->setLink("https://projet1.morimadrien.fr")
+                ->setCreatedAt(new \DateTime());
+
+        $src = "public/assets/images/works/portfolio-1.png";
+        $imageFile = new UploadedFile(
+            $src,
+            'portfolio-1.png',
+            'image/png',
+            filesize($src),
+            null,
+            true
+        );
+
+        $work->setImageFile($imageFile);
+        $work->setImageAlt("Couverture du site de l\'agence Web 'Webagency'");
+
+        $html = new Category();
+        $html->setName("HTML");
+
+        $manager->persist($html);
+
+        $work->addCategory($html);
+
+        $manager->persist($work);
+
+        // 2nd
+        $work = new Work();
+
+        $work->setTitle("Office du tourisme de Strasbourg")
+                ->setSubtitle("site de l'office de tourisme de Strasbourg")
+                ->setContent("Développeur HTML5/CSS3/Wordpress")
+                ->setLink("https://projet2.morimadrien.fr")
+                ->setCreatedAt(new \DateTime());
+
+        $src = "public/assets/images/works/portfolio-2.png";
+        $imageFile = new UploadedFile(
+            $src,
+            'portfolio-2.png',
+            'image/png',
+            filesize($src),
+            null,
+            true
+        );
+
+        $work->setImageFile($imageFile);
+        $work->setImageAlt("Couverture du site de l\'office de tourisme de Strasbourg");
+
+        $wp = new Category();
+        $wp->setName("Wordpress");
+
+        $manager->persist($wp);
+
+        $work->addCategory($html)
+             ->addCategory($wp);
+
+        $manager->persist($work);
+
+        // 3rd
+        $work = new Work();
+
+        $work->setTitle("Find a bike")
+                ->setSubtitle("Application Web de réservation de vélo")
+                ->setContent("Développeur HTML/CSS/JavaScript/jQuery.")
+                ->setLink("https://projet3.morimadrien.fr")
+                ->setCreatedAt(new \DateTime());
+
+        $src = "public/assets/images/works/portfolio-3.png";
+        $imageFile = new UploadedFile(
+            $src,
+            'portfolio-3.png',
+            'image/png',
+            filesize($src),
+            null,
+            true
+        );
+
+        $work->setImageFile($imageFile);
+        $work->setImageAlt("Couverture du site de l\'Application Web de réservation de vélo");
+
+        $js = new Category();
+        $js->setName("JS/jQuery");
+
+        $manager->persist($js);
+
+        $work->addCategory($html)
+             ->addCategory($js);
+
+        $manager->persist($work);
+
+        // 4th
+        $work = new Work();
+
+        $work->setTitle("Billet simple pour l'Alaska")
+                ->setSubtitle("Roman / blog de Jean Forteroche")
+                ->setContent("Développeur Web HTML/CSS/PHP/MySql/Bootstrap4.")
+                ->setLink("https://projet4.morimadrien.fr")
+                ->setCreatedAt(new \DateTime());
+
+        $src = "public/assets/images/works/portfolio-4.png";
+        $imageFile = new UploadedFile(
+            $src,
+            'portfolio-4.png',
+            'image/png',
+            filesize($src),
+            null,
+            true
+        );
+
+        $work->setImageFile($imageFile);
+        $work->setImageAlt("Couverture du site du Roman / blog de Jean Forteroche");
+
+        $php = new Category();
+        $php->setName("PHP/MySQL");
+
+        $manager->persist($php);
+
+        $work->addCategory($html)
+             ->addCategory($js)
+             ->addCategory($php);
+
+        $manager->persist($work);
+
+        // 5th
+        $work = new Work();
+
+        $work->setTitle("Portofolio")
+                ->setSubtitle("site CV")
+                ->setContent("Développeur Web HTML/CSS/Symfony4/PHP/MySql/Bootstrap4.")
+                ->setLink("https://morimadrien.fr")
+                ->setCreatedAt(new \DateTime());
+
+        $src = "public/assets/images/works/portfolio-5.png";
+        $imageFile = new UploadedFile(
+            $src,
+            'portfolio-5.png',
+            'image/png',
+            filesize($src),
+            null,
+            true
+        );
+
+        $work->setImageFile($imageFile);
+        $work->setImageAlt("Couverture du site du Roman / blog de Jean Forteroche");
+
+        $sf = new Category();
+        $sf->setName("Symfony4");
+
+        $manager->persist($sf);
+
+        $work->addCategory($html)
+             ->addCategory($js)
+             ->addCategory($php)
+             ->addCategory($sf);
+
+        $manager->persist($work);
 
         // On crée un admin pour l'accès au dashboard
         $user = new User();
